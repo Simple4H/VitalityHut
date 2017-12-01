084086ab1338bb5653912ec9f4de62bb184b5e00
S I M P L E <crescentcxm@gmail.com>
2017-12-01 13:08:36 +0800

    项目初始化

--- src/sql.sql
+++ src/sql.sql
@@ -18,9 +18,9 @@ CREATE TABLE `simple_user` (
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `simple_message` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '',
-  `title` varchar(100) NOT NULL COMMENT '',
-  `messges` varchar(1000) NOT NULL COMMENT '',
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '消息表id',
+  `title` varchar(100) NOT NULL COMMENT '标题',
+  `messges` varchar(1000) NOT NULL COMMENT '消息',
   `create_time` datetime NOT NULL COMMENT '创建时间',
   `update_time` datetime NOT NULL COMMENT '最后一次更新时间',
   PRIMARY KEY (`id`)
