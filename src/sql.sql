02bafafbed2c0d87e1a1c09fadf05bd6313a816f
S I M P L E <crescentcxm@gmail.com>
2017-12-01 13:07:53 +0800

    项目初始化

---
+++ src/sql.sql
@@ -0,0 +1,27 @@
+create database `db_vitalityhut`;
+
+use db_vitalityhut;
+
+CREATE TABLE `simple_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户表id',
+  `username` varchar(50) NOT NULL COMMENT '用户名',
+  `password` varchar(50) NOT NULL COMMENT '用户密码',
+  `email` varchar(50) DEFAULT NULL COMMENT '邮箱',
+  `phone` varchar(20) DEFAULT NULL COMMENT '手机',
+  `question` varchar(100) DEFAULT NULL COMMENT '找回密码问题',
+  `answer` varchar(100) DEFAULT NULL COMMENT '找回密码答案',
+  `role` int(4) NOT NULL COMMENT '角色0-管理员,1-普通用户',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `update_time` datetime NOT NULL COMMENT '最后一次更新时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_name_unique` (`username`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `simple_message` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '',
+  `title` varchar(100) NOT NULL COMMENT '',
+  `messges` varchar(1000) NOT NULL COMMENT '',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `update_time` datetime NOT NULL COMMENT '最后一次更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
