0bcd85b8a70cdfab92a7c42c5b55a2984c274e38
S I M P L E <crescentcxm@gmail.com>
2017-12-03 16:46:58 +0800

    消息

---
+++ src/main/java/com/simple/controller/MessageController.java
@@ -0,0 +1,12 @@
+package com.simple.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Create by S I M P L E on 2017/12/03
+ */
+@RequestMapping(value = "/message/")
+@Controller
+public class MessageController {
+}
