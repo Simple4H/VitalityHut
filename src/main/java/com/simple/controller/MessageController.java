828e3da4dbe45f0a4cf94800a6b29bbc9c9d8870
S I M P L E <crescentcxm@gmail.com>
2017-12-11 16:04:45 +0800

    修改分页的pageSize

--- src/main/java/com/simple/controller/MessageController.java
+++ src/main/java/com/simple/controller/MessageController.java
@@ -69,7 +69,7 @@ public class MessageController {
     //通过信息模糊查询
     @RequestMapping(value = "find_blur_message.do", method = RequestMethod.POST)
     @ResponseBody
-    public ServerResponse<PageInfo> findBlurMessage(@RequestBody Map map, HttpSession session, @RequestParam(value = "pageNum", defaultValue = "1") int pageNum, @RequestParam(value = "pageSize", defaultValue = "5") int pageSize) {
+    public ServerResponse<PageInfo> findBlurMessage(@RequestBody Map map, HttpSession session, @RequestParam(value = "pageNum", defaultValue = "1") int pageNum, @RequestParam(value = "pageSize", defaultValue = "10") int pageSize) {
         String messageCondition = (String) map.get("messageCondition");
         User currentUser = (User) session.getAttribute(Const.CURRENT_USER);
         if (currentUser == null) {
@@ -81,7 +81,7 @@ public class MessageController {
     //通过标题模糊查询
     @RequestMapping(value = "find_blur_title.do",method = RequestMethod.POST)
     @ResponseBody
-    public ServerResponse<PageInfo> findBlurTitle(@RequestBody Map map, HttpSession session, @RequestParam(value = "pageNum", defaultValue = "1") int pageNum, @RequestParam(value = "pageSize", defaultValue = "5") int pageSize) {
+    public ServerResponse<PageInfo> findBlurTitle(@RequestBody Map map, HttpSession session, @RequestParam(value = "pageNum", defaultValue = "1") int pageNum, @RequestParam(value = "pageSize", defaultValue = "10") int pageSize) {
         String titleCondition = (String) map.get("titleCondition");
         User currentUser = (User) session.getAttribute(Const.CURRENT_USER);
         if (currentUser == null) {
